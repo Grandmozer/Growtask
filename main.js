e2ba05d599058e0d6413e0a059a8a13e9b5779ad
Grandmozer <svetoskover@gmail.com>
2024-04-26 18:10:33 +0500

    scss

---
+++ main.js
@@ -0,0 +1,12 @@
+
+import './src/components/header/header.scss';
+import './src/components/buttons/buttons.scss';
+import './src/components/hero/hero.scss';
+
+//
+// import '/src/js/slider.js';
+// import '/src/js/galary.js.js';
+// import '/src/js/questions.js';
+// import '/src/js/smooth.js';
+// import '/src/js/main.js';
+//
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 let menuBtn = document.querySelector('.menu-btn'),
   menu = document.querySelector('.menu'),
-  li = document.querySelectorAll('.header-nav-item');
+  li = document.querySelectorAll('.header-nav-item'),
+    logo = document.getElementById('logo');
 
 menuBtn.addEventListener('click', function () {
   menuBtn.classList.toggle('active');
   menu.classList.toggle('active');
+  logo.classList.toggle('logo-menu');
+
+
 });
 
 li.forEach(function (button) {
