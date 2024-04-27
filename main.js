47e223a39cf042c775b70a33b06e8bb5de84393b
Grandmozer <svetoskover@gmail.com>
2024-04-27 18:05:38 +0500

    hero

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import './src/components/header/header.scss';
 import './src/components/buttons/buttons.scss';
 import './src/components/hero/hero.scss';
 
-//
-// import '/src/js/slider.js';
-// import '/src/js/galary.js.js';
-// import '/src/js/questions.js';
-// import '/src/js/smooth.js';
-// import '/src/js/main.js';
-//
+
+import '/src/js/slider.js';
+import '/src/js/galary.js';
+import '/src/js/questions.js';
+import '/src/js/smooth.js';
+import '/src/js/main.js';
+
